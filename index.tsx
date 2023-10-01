e06874a572082d5a7b689f53d63c7d05dadbc0d7
Colin Keenan <colinnkeenan@gmail.com>
2023-10-01 05:25:30 -0500

    Figured out how to do client-side React, but lost server-side stuff like Bun.write and Bun.file. I also have no way yet to handle routes so the form submit fails since /save does not exist. So far, the only server-side thing I tested was to make the Save button only show up onChange.

--- index.tsx
+++ index.tsx
@@ -1,37 +1,5 @@
-import { renderToReadableStream } from "react-dom/server";
-import Rentap from "./rentap"
+import { createRoot } from 'react-dom/client';
+import Rentap from "./rentap.tsx"
 
-const iconfile = Bun.file("icon.txt");
-const base64icon = await iconfile.text();
-const cssfile = Bun.file("style.css");
-const css = await cssfile.text();
-const aps = [new FormData()];
-
-const server = Bun.serve({
-  port: 4000,
-  async fetch(req) {
-    const url = new URL(req.url);
-
-    // render rentap.tsx for root path
-    if (url.pathname === "/") {
-      const stream = await renderToReadableStream(<Rentap icon={base64icon} css={css}/>);
-      return new Response(stream, {
-        headers: { "Content-Type": "text/html" },
-      });
-
-    }
-
-    // push formdata at /save into file store.json
-    if (url.pathname === "/save") {
-      const ap = await req.formData();
-      aps.push(ap);
-
-      await Bun.write("store.json", JSON.stringify(aps));
-      return new Response(JSON.stringify(aps));
-    }
-
-    return new Response("Not Found", { status: 404 });
-  },
-});
-
-console.log(`Listening on http://localhost:${server.port}`);
+const root = createRoot(document.getElementById('root'));
+root.render(<Rentap />)
--- rentap.tsx
+++ rentap.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Passing icon and css as workaround because I can't seem to import icon.png or figure out css in React
-function Rentap({icon, css}: {icon: string, css: string}) { 
+export default function Rentap() {
+
+  const [edited, setEdited] = useState(false);
+
+  const markFormEdited = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setEdited(true);
+  }
 
   return (
     <>
       <meta charSet="utf-8" />
       <title>Rental Application</title>
-      <link rel="icon" href={`data:image/x-icon;base64,${icon}`} />
-      <style dangerouslySetInnerHTML={{ __html: css }} />
+      <link rel="icon" type="image/png" href="icon.png" />
+      <link rel="stylesheet" type="text/css" href="styles.css" />
       <header>
-        <img src={`data:image/png;base64,${icon}`} alt="Rental Application Icon" />
+        <img src="icon.png" alt="Rental Application Icon" />
         <h1>Rental Application</h1>
       </header>
-      <form action="/save" method="post" encType="multipart/form-data">
+      <form action="/save" method="post" encType="multipart/form-data" onChange={markFormEdited} >
         <fieldset>
           <legend>Identity</legend>
-          <label htmlFor="fullname" className="personalinfo"> Full Name </label>{" "} <input type="text" name="FullName" id="fullname" placeholder="Full Name" />
-          <label htmlFor="ssnumber" className="personalinfo"> Social Security </label>{" "} <input type="text" name="SSN" id="ssnumber" placeholder="555-55-5555" pattern="\d{3}-\d{2}-\d{4}" />
-          <label htmlFor="birthdate" className="personalinfo"> Birth Date </label>{" "} <input type="date" name="BirthDate" id="birthdate" placeholder="Birth Date" />
-          <input type="text" name="MaritalStatus" id="maritalstatus" className="halfwidth" placeholder="Marital Status" />
-          <label htmlFor="email" className="personalinfo"> Email </label>{" "} <input type="email" name="Email" id="email" placeholder="Email" />
-          <label htmlFor="stateid" className="personalinfo"> State ID# </label>{" "} <input type="text" name="StateID" id="stateid" placeholder="State ID" />
-          <label htmlFor="phone1" className="personalinfo"> Phones </label>{" "} <input type="tel" name="Phone1" id="phone1" className="halfwidth" placeholder="Phone 1" />
-          <input type="tel" name="Phone2" id="phone2" className="halfwidth" placeholder="Phone 2" />
+          <label htmlFor="fullname"> Full Name </label>{" "}       <input type="text" name="FullName"      id="fullname" placeholder="Full Name" />
+          <label htmlFor="ssnumber"> Social Security </label>{" "} <input type="text" name="SSN"           id="ssnumber" placeholder="555-55-5555" pattern="\d{3}-\d{2}-\d{4}" />
+          <label htmlFor="birthdate"> Birth Date </label>{" "}     <input type="date" name="BirthDate"     id="birthdate" placeholder="Birth Date" />
+                                                                   <input type="text" name="MaritalStatus" id="maritalstatus" className="halfwidth" placeholder="Marital Status" />
+          <label htmlFor="email"> Email </label>{" "}             <input type="email" name="Email"         id="email" placeholder="Email" />
+          <label htmlFor="stateid"> State ID# </label>{" "}        <input type="text" name="StateID"       id="stateid" placeholder="State ID" />
+          <label htmlFor="phone1"> Phones </label>{" "}             <input type="tel" name="Phone1"        id="phone1" className="halfwidth" placeholder="Phone 1" />
+                                                                    <input type="tel" name="Phone2"        id="phone2" className="halfwidth" placeholder="Phone 2" />
           <textarea rows={5} name="CurrentAddress" id="currentaddress" placeholder="Address, City, State, Zip, Dates, Rent, Landlord name and phone number" defaultValue={""} />
           <textarea rows={17} name="PriorAddresses" id="prioraddresses" placeholder="Prior Addresses, Cities, States, Zips, Dates, Rents, Landlords" defaultValue={""} />
         </fieldset>
@@ -44,10 +50,10 @@ function Rentap({icon, css}: {icon: string, css: string}) {
         <label htmlFor="dateguested" className="personalinfo"> Date Guested </label>{" "} <input type="date" name="dateGuested" id="dateguested" />
         <label htmlFor="daterented" className="personalinfo"> Date Rented </label>{" "} <input type="date" name="dateRented" id="daterented" />
         <input type="text" name="headerName" id="headerName" placeholder="Header Name" />
-        <input type="submit" defaultValue="Save" />
+        <div>
+          {edited && <input type="submit" defaultValue="Save" />}
+        </div>
       </form>
     </>
   );
 }
-
-export default Rentap;
