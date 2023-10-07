f60143ffb75314f883bfe696a22366e1a9ca91a0
Colin Keenan <colinnkeenan@gmail.com>
2023-10-07 12:59:21 -0500

    reads store.json, allows adding new entries, saves store.json

--- back.tsx
+++ back.tsx
@@ -3,7 +3,10 @@ import Rentap from "./rentap"
 
 const iconfile = Bun.file("icon.txt");
 const base64icon = await iconfile.text();
-const aps = [new FormData()];
+const storefile = Bun.file("store.json");
+const apjsonstring = storefile.size ? await storefile.text() : "";
+const aps = apjsonstring ? JSON.parse(apjsonstring)
+  : [{"FullName":"","SSN":"","BirthDate":"","MaritalStatus":"","Email":"","StateID":"","Phone1":"","Phone2":"","CurrentAddress":"","PriorAddresses":"","ProposedOccupants":"","ProposedPets":"","Income":"","Employment":"","Evictions":"","Felonies":"","dateApplied":"","dateGuested":"","dateRented":"","headerName":""}];
 
 const server = Bun.serve({
   port: 3000,
@@ -12,20 +15,25 @@ const server = Bun.serve({
 
     // render rentap.tsx for root path
     if (url.pathname === "/") {
-      const stream = await renderToReadableStream(<Rentap icon={base64icon} />);
+      const stream = await renderToReadableStream(<Rentap icon={base64icon} message="New" color="red" ap={aps[0]}/>);
       return new Response(stream, {
         headers: { "Content-Type": "text/html" },
       });
-
     }
 
     // push formdata at /save into file store.json
     if (url.pathname === "/save") {
-      const ap = await req.formData();
-      aps.push(ap);
-
-      await Bun.write("./store.json", JSON.stringify(aps));
-      return new Response(JSON.stringify(aps));
+      const formData = await req.formData();
+      const apNew = Object.fromEntries(formData.entries());
+      aps.push(apNew);
+      // write each ap on it's own line
+      const [a0, ...aRest] = aps;
+      const formattedAps = `[${JSON.stringify(a0)},${aRest.map( (a) => "\n" + JSON.stringify(a) )}]`;
+      await Bun.write("./store.json", formattedAps);
+      const stream = await renderToReadableStream(<Rentap icon={base64icon} message="Saved" color="green" ap={apNew}/>);
+      return new Response(stream, {
+        headers: { "Content-Type": "text/html" },
+      });
     }
 
     return new Response("Not Found", { status: 404 });
--- rentap.tsx
+++ rentap.tsx
@@ -1,6 +1,10 @@
+//each fieldset acts like a column
+//set all columns next to each other if possible
 const fieldsetStyle={display:'inline-block', width:'425px', border:'none'}
 //other styles defined inline or in functions that follow this Rentap function
-export default function Rentap({icon}: {icon: string}) {
+
+export default function Rentap({icon, message, color, ap}: { icon: string, message: string, color: string, ap: {[key: string]: any} } ) {
+
 
   return (
     <>
@@ -10,42 +14,46 @@ export default function Rentap({icon}: {icon: string}) {
       <header>
         <img src={`data:image/png;base64,${icon}`} alt="Rental Application Icon" />
         <h1 style={{display:'inline-block'}}>Rental Application</h1>
+        <Space />
+        <h3 style={{display:'inline-block', color:color}}>{message}</h3>
       </header>
       <body style={{backgroundColor:'paleturquoise'}} >
       <form action="/save" method="post" encType="multipart/form-data" >
         <fieldset style={fieldsetStyle}>
           <legend>Identity</legend>
-          <Label forId="fullname"  labelText="Full Name" />       <IdField type="text"  name="FullName"      placeholder="First Middle Last" />
-          <Label forId="ssn"  labelText="Social Security" />      <IdField type="text"  name="SSN"           placeholder="555-55-5555" />
-          <Label forId="birthdate" labelText="Birth Date" />    <HalfField type="date"  name="BirthDate"     placeholder=""/>
-                                                                <HalfField type="text"  name="MaritalStatus" placeholder="Marital Status" />
-          <Label forId="email" labelText="Email" />               <IdField type="email" name="Email"         placeholder="youremail@provider.com" />
-          <Label forId="stateid" labelText="State ID#" />         <IdField type="text"  name="StateID"       placeholder="MO 123456789 1/2/2034" />
-          <Label forId="phone1" labelText="Phones" />           <HalfField type="tel"   name="Phone1"        placeholder="Phone 1" />
-                                                                <HalfField type="tel"   name="Phone2"        placeholder="Phone 2" />
-          <TextArea rows={5}  name="CurrentAddress"    placeholder="Address, City, State, Zip, Dates, Rent, Landlord name and phone number" />
-          <TextArea rows={17} name="PriorAddresses"    placeholder="Prior Addresses, Cities, States, Zips, Dates, Rents, Landlords" />
+          <Label forId="fullname"  labelText="Full Name" />     <Field type="text"  name="FullName"      placeholder="First Middle Last" width='76%' ap={ap} />
+          <Label forId="ssn"  labelText="Social Security" />    <Field type="text"  name="SSN"           placeholder="555-55-5555" width='76%' ap={ap} />
+          <Label forId="birthdate" labelText="Birth Date" />    <Field type="date"  name="BirthDate"     placeholder="" width='38%' ap={ap} />
+                                                                <Field type="text"  name="MaritalStatus" placeholder="Marital Status" width='38%' ap={ap} />
+          <Label forId="email" labelText="Email" />             <Field type="email" name="Email"         placeholder="youremail@provider.com" width='76%' ap={ap} />
+          <Label forId="stateid" labelText="State ID#" />       <Field type="text"  name="StateID"       placeholder="MO 123456789 1/2/2034" width='76%' ap={ap} />
+          <Label forId="phone1" labelText="Phones" />           <Field type="tel"   name="Phone1"        placeholder="Phone 1" width='38%' ap={ap} />
+                                                                <Field type="tel"   name="Phone2"        placeholder="Phone 2" width='38%' ap={ap} />
+          <TextArea rows={5}  name="CurrentAddress"    placeholder="Address, City, State, Zip, Dates, Rent, Landlord name and phone number" ap={ap} />
+          <TextArea rows={17} name="PriorAddresses"    placeholder="Prior Addresses, Cities, States, Zips, Dates, Rents, Landlords" ap={ap} />
         </fieldset>
         <fieldset style={fieldsetStyle}>
           <legend>Living Situation</legend>
-          <TextArea rows={5}  name="ProposedOccupants" placeholder="Proposed Occupants: self+age, other+age" />
-          <TextArea rows={3}  name="ProposedPets"      placeholder="Proposed Pets, names, types, ages, weights" />
-          <TextArea rows={6}  name="Income"            placeholder="Income amount and source" />
-          <TextArea rows={15} name="Employment"        placeholder="Employment: address, job/position, dates, hours, supervisor name and phone number" />
+          <TextArea rows={5}  name="ProposedOccupants" placeholder="Proposed Occupants: self+age, other+age" ap={ap} />
+          <TextArea rows={3}  name="ProposedPets"      placeholder="Proposed Pets, names, types, ages, weights" ap={ap} />
+          <TextArea rows={6}  name="Income"            placeholder="Income amount and source" ap={ap} />
+          <TextArea rows={15} name="Employment"        placeholder="Employment: address, job/position, dates, hours, supervisor name and phone number" ap={ap} />
         </fieldset>
         <fieldset style={fieldsetStyle}>
           <legend>Criminal &amp; Civil Record | Notes</legend>
-          <TextArea rows={15} name="Evictions"         placeholder="Evictions Past 10 Years, or other notes" />
-          <TextArea rows={15} name="Felonies"          placeholder="Felonies/Drug Convictions, or other notes" />
+          <TextArea rows={15} name="Evictions"         placeholder="Evictions Past 10 Years, or other notes" ap={ap} />
+          <TextArea rows={15} name="Felonies"          placeholder="Felonies/Drug Convictions, or other notes" ap={ap} />
         </fieldset>
         <br />
-        <Label forId="dateapplied" labelText="Date Applied"/> <input type="date" name="dateApplied" id="dateapplied" />
-        <Label forId="dateguested" labelText="Date Guested"/> <input type="date" name="dateGuested" id="dateguested" />
-        <Label forId="daterented" labelText="Date Rented"  /> <input type="date" name="dateRented"  id="daterented" />
+        <Label forId="dateapplied" labelText="Date Applied"/> <Field type="date" name="dateApplied" placeholder="" width='auto' ap={ap} />
+        <Label forId="dateguested" labelText="Date Guested"/> <Field type="date" name="dateGuested" placeholder="" width='auto' ap={ap} />
+        <Label forId="daterented" labelText="Date Rented"  /> <Field type="date" name="dateRented"  placeholder="" width='auto' ap={ap} />
         <Space />
-        <input type="text" name="headerName" id="headername" placeholder="Header Name" />
+        <Field type="text" name="headerName" placeholder="Header Name" width='auto' ap={ap} />
         <Space />
         <input type="submit" defaultValue="Save" style={{backgroundColor:'lightgreen'}} />
+        <Space />
+        <a href="/">New</a>
       </form>
       </body>
     </>
@@ -60,21 +68,19 @@ function Label({forId, labelText}: {forId:string, labelText:string}) {
   )
 }
 
-function IdField({type, name, placeholder}: {type: string, name: string, placeholder: string}) {
+function Field({type, name, placeholder, width, ap}: { type: string, name: string, placeholder: string, width: string, ap: {[key:string]: any} }) {
   return (
-    <input type={type} name={name} id={name.toLowerCase()} placeholder={placeholder} style={{width:'76%'}} />
+    ap[name] ?
+    <input type={type} name={name} id={name.toLowerCase()} placeholder={placeholder} style={{width:width}} value={ap[name]} readOnly={true}/>
+    : <input type={type} name={name} id={name.toLowerCase()} placeholder={placeholder} style={{width:width}} />
   )
 }
 
-function HalfField({type, name, placeholder}: {type: string, name: string, placeholder: string}) {
+function TextArea({rows, name, placeholder, ap}: { rows:number, name:string, placeholder:string, ap: {[key:string]: any} }) {
   return (
-    <input type={type} name={name} id={name.toLowerCase()} placeholder={placeholder} style={{width:'38%'}} />
-  )
-}
-
-function TextArea({rows, name, placeholder}: {rows:number, name:string, placeholder:string }) {
-  return (
-    <textarea rows={rows} name={name} placeholder={placeholder} defaultValue="" style={{width:'100%'}} />
+    ap[name] ?
+    <textarea rows={rows} name={name} placeholder={placeholder} style={{width:'100%'}} defaultValue={ap[name]} readOnly={true}/>
+    : <textarea rows={rows} name={name} placeholder={placeholder} style={{width:'100%'}} />
   )
 }
 
